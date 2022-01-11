7b63425eac7d4cbcfb3f6e9228e3d709ced079d1
srikanthraop <parcha.srikanthrao@gmail.com>
2022-01-11 15:53:48 +0530

    Add footer, header, about and a different todolist-profile

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // app.use(express.static("public"));
 app.use(express.static(__dirname + '/public'));
 
+let workItems = [];
 let items = ["Srikanth", "Rao", "Parcha"];
 
 app.get("/", function(req, res){
@@ -21,17 +22,44 @@ app.get("/", function(req, res){
 
   let day = today.toLocaleDateString("en-US", options)
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 
 })
 
 app.post("/", function(req, res){
+  // console.log(req.body);
   let item = req.body.newItem
-  items.push(item);
-  res.redirect("/");
+
+  if (req.body.list == "Work List"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
 })
+
+// ///////////work
+app.get("/work", function(req, res){
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  })
+})
+
+app.post("/work", function(req, res){
+  console.log(req.body);
+  let workItem = req.body.newItem;
+  workItems.push(workItem);
+  res.redirect("/work");
+})
+
+app.get("/about", function(req, res){
+  res.render("about");
+})
+
 
 app.listen(3000, function(){
   console.log("The server is up and running om port 3000");
